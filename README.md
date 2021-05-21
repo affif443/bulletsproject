4684abe606132ac1f2de141f9b7a9cd8e967794d
affif443 <80408741+affif443@users.noreply.github.com>
2021-05-21 19:15:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bulletsproject
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,59 @@
+var bullet,speed;
+var weight,wall;
+var thickness;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+
+  bullet = createSprite(50,200,50,10);
+  wall= createSprite(1200,200,thickness,100);
+
+  speed=random(40,90);
+  weight=random(500,1500);
+  thickness=random(20,80);
+
+  car.velocityX=speed;
+}
+
+function draw() {
+  background("black");
+  
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2)
+  bullet.velocityX=0;
+  var deformation=0.5*speed*weight*speed/22509;
+  if(deformation > 180){
+    bullet.shapeColor= color("red");
+  }
+  if(deformation<180 && deformation>100){
+    bullet.shapeColor= color("yellow");
+  }
+  if(deformation < 100){
+    bullet.shapeColor= color("green");
+  }
+
+
+
+  drawSprites();
+}
+
+
+function collided(lbullet,lwall){
+
+  bulletRightEdge= lbullet.x + lbullet.width;
+  wallRightEdge=lwall.width;
+
+  if(bulletRightEdge>=wallRightEdge){
+    return true;
+  }
+    return false;
+}
+    
+
+}
+
+
+
+
